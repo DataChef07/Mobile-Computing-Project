7c717144960f8329cc9da91c511c1adf3e83b10b
Tarun-kumar-gupta999 <tarun22082@iiitd.ac.in>
2023-04-30 01:31:22 +0530

    Update MainActivity2.java

--- app/src/main/java/com/example/startupconnect/MainActivity2.java
+++ app/src/main/java/com/example/startupconnect/MainActivity2.java
@@ -120,7 +120,7 @@ public class MainActivity2 extends AppCompatActivity {
         Intent intent = getIntent();
         userName = intent.getStringExtra(LoginActivity.EXTRA_USERNAME);
         String timeStamp = new SimpleDateFormat("yyyy.MM.dd.HH.mm").format(new java.util.Date());
-        QR_String = userName + "$"+ sport +"$"+ timeStamp;
+        QR_String = userName + "--"+ sport +"--"+ timeStamp;
         qrgEncoder = new QRGEncoder(QR_String, null, QRGContents.Type.TEXT, dim);
         try{
             bitmap = qrgEncoder.getBitmap();

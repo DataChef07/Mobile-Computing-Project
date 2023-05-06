956ea878163926f6f150006ef4a98a4def71425f
Tarun-kumar-gupta999 <tarun22082@iiitd.ac.in>
2023-05-07 02:59:25 +0530

    Update Recycle_loginandreg.java

--- app/src/main/java/com/example/startupconnect/Recycle_loginandreg.java
+++ app/src/main/java/com/example/startupconnect/Recycle_loginandreg.java
@@ -2,6 +2,7 @@ package com.example.startupconnect;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 

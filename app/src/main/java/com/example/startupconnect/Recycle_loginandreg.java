b590945020c35a898f0bf7381d5af9b306153d8b
Tarun-kumar-gupta999 <tarun22082@iiitd.ac.in>
2023-05-07 03:12:15 +0530

    Revert "Update Recycle_loginandreg.java"
    This reverts commit 956ea878163926f6f150006ef4a98a4def71425f.

--- app/src/main/java/com/example/startupconnect/Recycle_loginandreg.java
+++ app/src/main/java/com/example/startupconnect/Recycle_loginandreg.java
@@ -2,7 +2,6 @@ package com.example.startupconnect;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
